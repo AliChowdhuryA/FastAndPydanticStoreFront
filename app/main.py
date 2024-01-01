21c28624fe1cea504dbe7659244dfccded708bae
Ali Chowdhury <72588555+AliChowdhuryA@users.noreply.github.com>
2024-01-01 10:43:51 -0500

    update: learned fastAPI

--- app/main.py
+++ app/main.py
@@ -1,9 +1,52 @@
 # app/main.py
 
+from enum import Enum
+
 from fastapi import FastAPI
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+def root():
     return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id:str):
+    return {"user_id": user_id}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    try:
+        with open(file_path, "r") as file:
+            content = file.read()
+        return {"file_path": file_path, "content": content}
+    except FileNotFoundError:
+        return {"file_path": file_path, "error": "File not found"}
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
